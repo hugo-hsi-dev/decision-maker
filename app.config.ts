2dd72b95d96ea72d71c16fcf658d34c37c1ca1fd
Hugo Hsi <hugohsidev@gmail.com>
2025-02-14 13:08:05 -0500

    updates app config for bun deployment

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from '@tanstack/start/config'
-import tsConfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from '@tanstack/start/config';
+import tsConfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+  server: {
+    preset: 'bun',
+  },
   vite: {
     plugins: [
       tsConfigPaths({
@@ -9,4 +12,4 @@ export default defineConfig({
       }),
     ],
   },
-})
+});
