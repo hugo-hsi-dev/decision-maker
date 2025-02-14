6e539b0c30433de43735c2721225271da1c360c9
Hugo Hsi <hugohsidev@gmail.com>
2025-02-14 15:50:20 -0500

    about to lint fix all errors

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,15 +1,13 @@
-import { Button } from '@/components/ui/button';
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
-	component: Home,
-});
+  component: Home,
+})
 
 function Home() {
-	return (
-		<div className='p-2'>
-			<h3>Welcome Home!!!</h3>
-			<Button>I am a button</Button>
-		</div>
-	);
+  return (
+    <div className="p-2">
+      <h3>Welcome Home!!!</h3>
+    </div>
+  )
 }
