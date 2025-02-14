b4a0e5b945250af34c8ecee2af9ea5fff981e6c1
Hugo Hsi <hugohsidev@gmail.com>
2025-02-14 15:35:14 -0500

    need to fix styles import

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button';
 import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/')({
@@ -8,6 +9,7 @@ function Home() {
 	return (
 		<div className='p-2'>
 			<h3>Welcome Home!!!</h3>
+			<Button>I am a button</Button>
 		</div>
 	);
 }
